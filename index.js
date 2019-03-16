7bd0918d0022e8560728bf0177d9ecd5000ffd6f
Blake Stoner <stonerblake@gmail.com>
2019-03-16 15:35:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  const arr = []
+
+  for (let i = 0, l = musicians.length; i < l; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+
+  return arr
+}
+
+function johnLennonFacts(facts) {
+  const jlFacts = []
+
+  let i = 0
+  while (i < facts.length) {
+    jlFacts.push(`${facts[i]}!!!`)
+    i++
+  }
+
+  return jlFacts
+}
+
+function iLoveTheBeatles(n) {
+  const arr = []
+
+  do {
+    arr.push('I love the Beatles!')
+    n++
+  } while (n < 15)
+
+  return arr
+} 
